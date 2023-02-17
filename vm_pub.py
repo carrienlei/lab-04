6d46f61ead90c3efe46dc16f58f314ecabd7c97a
Carrie Lei <122946892+carrienlei@users.noreply.github.com>
2023-02-17 15:05:51 -0800

    Update vm_pub.py

--- vm_pub.py
+++ vm_pub.py
@@ -1,5 +1,11 @@
 """EE 250L Lab 04 Starter Code
-Run vm_sub.py in a separate terminal on your VM."""
+
+Carrie Lei
+Ishraq Rahman 
+https://github.com/carrienlei/lab-04
+
+"""
+
 
 import paho.mqtt.client as mqtt
 import time
